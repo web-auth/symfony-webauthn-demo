de845a24dc53b9b6f2854578de1822bb1ccc7b4f
Spomky <florent@morselli.fr>
2019-03-19 13:15:43 +0100

    v1.1-alpha 1 and useless classes removed

--- config/routes/routes.php
+++ config/routes/routes.php
@@ -5,5 +5,4 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
-    $routes->import('.', 'webauthn');
 };
