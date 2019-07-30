2d5a981e88576b33a51fc3264802a88974160e3c
Spomky <florent@morselli.fr>
2019-07-30 17:53:44 +0200

    Bug fixed

--- config/routes/routes.php
+++ config/routes/routes.php
@@ -15,28 +15,39 @@ return function (RoutingConfigurator $routes) {
 
     $routes->add('api_attestation_request', '/api/register/options')
         ->controller(AttestationRequestController::class)
-        ->methods(['POST']);
+        ->methods(['POST'])
+        ->schemes(['https'])
+    ;
     $routes->add('api_attestation_response', '/api/register')
         ->controller(AttestationResponseController::class)
-        ->methods(['POST']);
+        ->methods(['POST'])
+        ->schemes(['https'])
+    ;
 
 // Profile API
     $routes->add('api_profile', '/api/profile')
         ->controller(ProfileController::class)
-        ->methods(['GET']);
+        ->methods(['GET'])
+        ->schemes(['https'])
+    ;
 
     $routes->add('api_change_display_name', '/api/change_display_name')
         ->controller(ChangeDisplayNameController::class)
-        ->methods(['PUT']);
+        ->methods(['PUT'])
+        ->schemes(['https'])
+    ;
 
 // Logout API
     $routes->add('api_logout', '/api/logout')
         ->controller(LogoutController::class)
-        ->methods(['GET']);
+        ->methods(['GET'])
+        ->schemes(['https'])
+    ;
 
 // Home
     $routes->add('app_home', '/{reactRouting}')
         ->controller(HomepageController::class)
         ->requirements(['reactRouting' => '^(?!_).*$'])
-        ->defaults(['reactRouting' => null]);
+        ->defaults(['reactRouting' => null])
+    ;
 };
--- public/index.php
+++ public/index.php
@@ -12,22 +12,16 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
+$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
+if($_SERVER['APP_ENV'] === 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+if($trustedProxies) {
+    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
-Request::setTrustedProxies(
-    ['127.0.0.1', $request->server->get('REMOTE_ADDR')],
 
-    // only trust X-Forwarded-Port/-Proto, not -Host
-    Request::HEADER_X_FORWARDED_AWS_ELB
-);
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
