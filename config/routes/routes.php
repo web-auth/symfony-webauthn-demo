36ee002d04df1a2f1fb8cd7dbf37f16cdb5bb7ad
Spomky <florent@morselli.fr>
2019-07-30 16:04:32 +0200

    Webauthn routes

--- config/routes/routes.php
+++ config/routes/routes.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
+    $routes->import('.', 'webauthn');
 };
