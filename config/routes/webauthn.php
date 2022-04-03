e35e5f2a3c86e9e4736078d49c7d4b0ead6be55c
Florent Morselli <contact@spomky-labs.com>
2022-04-03 17:07:39 +0200

    Project updated

--- config/routes/webauthn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-
-return static function (RoutingConfigurator $routes) {
-    $routes->import('.', 'webauthn');
-};
