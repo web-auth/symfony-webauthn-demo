ae645a5533ce20bc51282dda4ba2cc8d4b90fba4
Florent Morselli <florent.morselli@spomky-labs.com>
2023-10-01 18:06:27 +0200

    Download assets

--- importmap.php
+++ importmap.php
@@ -22,12 +22,15 @@ return [
         'path' => '@symfony/stimulus-bundle/loader.js',
     ],
     '@hotwired/stimulus' => [
+        'downloaded_to' => 'vendor/@hotwired/stimulus.js',
         'url' => 'https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.2/+esm',
     ],
     'typed.js' => [
+        'downloaded_to' => 'vendor/typed.js.js',
         'url' => 'https://cdn.jsdelivr.net/npm/typed.js@2.0.132/+esm',
     ],
     '@simplewebauthn/browser' => [
+        'downloaded_to' => 'vendor/@simplewebauthn/browser.js',
         'url' => 'https://cdn.jsdelivr.net/npm/@simplewebauthn/browser@8.2.1/+esm',
     ],
 ];
